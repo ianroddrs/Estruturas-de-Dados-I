2d0acb89380997c089d1a84a7c924875e4072ebb
Ian Rodrigues <86446951+ianroddrs@users.noreply.github.com>
2023-03-16 19:59:05 -0300

    Add files via upload

---
+++ hello/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main() {
+	float media = 10;
+	cout << "ola " << media << " texto" << endl;
+	return 0;
+}
