282112225272050c2d8d52b7c98e89f1185f5559
ianroddrs <mateusroddi@gmail.com>
2023-03-26 15:38:19 -0300

    moved to other dir

--- CODES/TAD_CB/conta.h
+++
@@ -1,8 +0,0 @@
-typedef struct{
-    int numero;
-    double saldo;
-} TipoConta;
-
-TipoConta inicializar(int n, double sal);
-void deposito(TipoConta *conta, double valor);
-void print(TipoConta conta);
--- CODES/hello/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	float media;
-	cin >> media;
-	cout << "a media de pontos e: " << media << " pontos" << endl;
-	return 0;
-}
